795f3cb3a3ac5d2f18ca4e07bb3d09aa58152ec3
ajmalmohad <ajmalajmal.2016@gmail.com>
2024-08-17 12:34:25 +0530

    feat: gorm model

--- platform/auth/models/tokens.go
+++ platform/auth/models/tokens.go
@@ -2,14 +2,15 @@ package models
 
 import (
     "time"
+    "gorm.io/gorm"
 )
 
 type RefreshToken struct {
-    ID        uint           `gorm:"primaryKey"`
+    gorm.Model
+
     UserID    uint           `gorm:"not null;index"`
     Token     string         `gorm:"type:text;not null;unique"`
     ExpiresAt time.Time      `gorm:"not null"`
-    CreatedAt time.Time      `gorm:"autoCreateTime"`
 	
     User      User           `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
--- platform/auth/models/users.go
+++ platform/auth/models/users.go
@@ -1,15 +1,14 @@
 package models
 
 import (
-    "time"
+    "gorm.io/gorm"
 )
 
 type User struct {
-    ID        uint           `gorm:"primaryKey"`
+    gorm.Model
+
     Name      string         `gorm:"size:255;not null"`
     Email     string         `gorm:"size:255;unique;not null"`
 	Password  string         `gorm:"size:255;not null"`
-    CreatedAt time.Time		 `gorm:"autoCreateTime"`
-    UpdatedAt time.Time		 `gorm:"autoUpdateTime"`
 	IsActive  bool           `gorm:"default:true"`
 }
